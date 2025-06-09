f100dd8643721a78073c25c1393d8f16b6cc51c1
Oleg Frolov <frolololeg@gmail.com>
2025-06-09 22:09:54 +0100

    ^^

--- Runtime/Shaders/Toggle.hlsl
+++ Runtime/Shaders/Toggle.hlsl
@@ -16,15 +16,15 @@ void ToggleBgColor_float(
 void ToggleVertexOffset_float(
     float Value,
     float Length,
-    float4 LocalPosition,
+    float4 Right,
     float4 VertexColor,
     float3 ObjectPosition,
     out float3 Out)
 {
     float yFactor = Value < 0.5 ? Value * 2.0 : 1.0;
     float zFactor = Value > 0.5 ? (Value - 0.5) * 2.0 : 0.0;
-    float3 posY = LocalPosition.xyz * -Length * yFactor;
-    float3 posZ = LocalPosition.xyz * -Length * zFactor;
+    float3 posY = Right.xyz * -Length * yFactor;
+    float3 posZ = Right.xyz * -Length * zFactor;
     float3 pos = VertexColor.y > 0.9 ? ObjectPosition + posY : ObjectPosition;
     pos = VertexColor.z > 0.9 ? pos + posZ : pos;
     Out = pos;
