53f98c8da0132a199d9d0d4ff11ba7d5daeeccc6
Oleg Frolov <frolololeg@gmail.com>
2025-07-08 22:38:41 +0100

    button as a prefab

--- Runtime/Scripts/Button.cs
+++ Runtime/Scripts/Button.cs
@@ -1,19 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Volorf.VolumeUI
 {
     [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer), typeof(BoxCollider))]
     public class Button : VolumeUIBehaviour
     {
+        [Tooltip("In millimeters")]
+        [SerializeField] float _width = 120f;
+        
         [Header("Animation")]
         [SerializeField] float _duration = 1.0f;
         [SerializeField] AnimationCurve _curve = AnimationCurve.EaseInOut(0, 0, 1, 1);
         
         [Header("Style")] 
         public Color bodyColor;
-        public Color buttonColor;
+        public Color buttonDefaultColor;
+        public Color buttonPressedColor;
+        
+        [Header("Elements")]
+        [SerializeField] MeshRenderer _meshRenderer;
+
+        public UnityEvent onTap;
+        
+        MaterialPropertyBlock _mpb;
+        Coroutine _animateCoroutine;
+        float _currentValue;
+        bool _isOnOffAnimating;
     }
 }
 
